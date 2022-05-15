faa06a8e55ddc0e662459ce8555b7af7b772630d
NatalyaVas <kirnata@mail.ru>
2022-05-15 11:30:29 +0300

    Changed consts. hw1

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // const colors = require('colors');
 import colors from 'colors'; //для использования es6 module сделать запись в package.json
 
-const leftNum = process.argv.slice(2);
-const rightNum = process.argv.slice(3);
+const leftNum = +(process.argv[2]);
+const rightNum = +(process.argv[3]);
 
-if(isNan(leftNum) || isNan(rightNum)){
-    console.log(colors.red('Введены некорректные параметры'));
-}
+// if(isNan(leftNum) || isNan(rightNum)){
+//     console.log(colors.red('Введены некорректные параметры'));
+// }
 
 for (let i = leftNum; i <= rightNum; i++) {
 	if (leftNum % i === 0 && rightNum % i === 0) {
 		// break;
 		console.log('Ошибка. Введите числа от 2');
 	} else {
+		console.log(colors.red(i));
 		console.log(colors.green(i));
 		console.log(colors.yellow(i));
-		console.log(colors.red(i));
 	}
 }
