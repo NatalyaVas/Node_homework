97028571f341e9dc50c4153cdd01d334f86b513b
NatalyaVas <kirnata@mail.ru>
2022-05-17 17:57:22 +0300

    hw1

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
+'use strict';
+
 // const colors = require('colors');
 import colors from 'colors'; //для использования es6 module сделать запись в package.json
 
 const leftNum = +(process.argv[2]);
 const rightNum = +(process.argv[3]);
 
-// if(isNan(leftNum) || isNan(rightNum)){
-//     console.log(colors.red('Введены некорректные параметры'));
-// }
-
-for (let i = leftNum; i <= rightNum; i++) {
-	if (leftNum % i === 0 && rightNum % i === 0) {
-		// break;
-		console.log('Ошибка. Введите числа от 2');
-	} else {
-		console.log(colors.red(i));
-		console.log(colors.green(i));
-		console.log(colors.yellow(i));
+function noSimpleNum () {
+	if(isNaN(leftNum) || isNaN(rightNum)){
+		console.log(colors.red('Введенные данные не являются числом'));
+		return true;
 	}
+	if(leftNum > rightNum) {
+		console.log(colors.red('Ошибка. Первое число должно быть меньше второго.'));
+		return true;
+	}
+	if(!leftNum || 0 === leftNum.length || !rightNum || 0 === rightNum.length) {
+		console.log(colors.red('Данные отсутствуют. Введите два числа.'));
+		return true;
+	}
+	return false;
 }
+
+function createColors(numbers) {
+	if (noSimpleNum())
+		return false;
+	let color = 'green';
+	numbers.forEach(number => {
+		switch (color) {
+			case 'green':
+				console.log(colors.green(number));
+				color = 'yellow';
+				break;
+			case 'yellow':
+				console.log(colors.yellow(number));
+				color = 'red';
+				break;
+			case 'red':
+				console.log(colors.red(number));
+				color = 'green';
+				break;
+		}
+	});
+}
+createColors(leftNum, rightNum)
